e0917844fbf98b77b4ea9a424b9e1ab8dae009b0
Justin Klaver <Klaverts@yahoo.com>
2022-11-14 09:16:12 -0500

    adding api routes

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { User } = require('./models');
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/userdb',
@@ -17,30 +18,31 @@ mongoose.connect(
   }
 );
 
+
+// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-// Create a new notebook
-app.post('/submit', ({ body }, res) => {
-    User.create(body)
-      .then(dbUserData => {
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-  
-  // Retrieve all users
-  app.get('/users', (req, res) => {
-    User.find()
-      .then(dbUserData => {
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+// // Create a new notebook
+// app.post('/submit', ({ body }, res) => {
+//     User.create(body)
+//       .then(dbUserData => {
+//         res.json(dbUserData);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+  
+//   // Retrieve all users
+//   app.get('/users', (req, res) => {
+//     User.find()
+//       .then(dbUserData => {
+//         res.json(dbUserData);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
